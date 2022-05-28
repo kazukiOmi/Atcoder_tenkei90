20e0d95619d606f470e2e783ebd698381591e164
Kazuki Omi <k.omi.646@stn.nitech.ac.jp>
2022-05-29 01:55:33 +0900

    区間累積和

--- tmp/main.py
+++ tmp/main.py
@@ -1,28 +1,44 @@
 import sys
 
-
 n = int(input())
-rate_list = list(map(int, input().split()))
-# rate_list = [869120000, 998244353, 777777777, 123456789, 100100100,
-#              464646464, 987654321, 252525252, 869120001, 1000000000]
-rate_list.sort()
-num_stu = int(input())
 
-for i in range(num_stu):
-    left = 0
-    right = n-1
-    mid = n//2
-    rate = int(input())
-    if rate_list[left] == rate or rate_list[right] == rate:
-        print(0)
-        continue
-    while(right-left) > 1:
-        if rate < rate_list[mid]:
-            right = mid
-        elif rate > rate_list[mid]:
-            left = mid
-        else:
-            right = mid
-            break
-        mid = (left+right)//2
-    print(min(abs(rate_list[left]-rate), abs(rate_list[right]-rate)))
+one_list = []
+two_list = []
+
+sum_one = 0
+sum_two = 0
+one_list.append(0)
+two_list.append(0)
+for i in range(n):
+    cls, score = list(map(int, input().split()))
+    if cls == 1:
+        sum_one += score
+    else:
+        sum_two += score
+    one_list.append(sum_one)
+    two_list.append(sum_two)
+
+q = int(input())
+
+for i in range(q):
+    start, end = list(map(int, input().split()))
+    print(
+        f"{one_list[end]-one_list[start-1]} {two_list[end]-two_list[start-1]}")
+
+
+# one_list = []
+# total_list = []
+# for i in range(n):
+#     cls, score = list(map(int, input().split()))
+#     if cls == 1:
+#         one_list.append(score)
+#     else:
+#         one_list.append(0)
+#     total_list.append(score)
+# q = int(input())
+
+# for i in range(q):
+#     start, end = list(map(int, input().split()))
+#     sum_one = sum(one_list[start-1:end])
+#     sum_total = sum(total_list[start-1:end])
+#     print(f"{sum_one} {sum_total-sum_one}")
