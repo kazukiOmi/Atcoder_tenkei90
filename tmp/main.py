a8dd2233b0006412bed0059270f92ad8f1112fef
Kazuki Omi <k.omi.646@stn.nitech.ac.jp>
2022-05-30 00:51:08 +0900

    38: Large LCM

--- tmp/main.py
+++ tmp/main.py
@@ -1,20 +1,13 @@
 import sys
+# import numpy as np
+import math
 
 input = sys.stdin.readline
 
-n, k = list(map(int, input().split()))
-a_list = list(map(int, input().split()))
-b_list = list(map(int, input().split()))
-diff_list = [abs(a_list[i]-b_list[i]) for i in range(n)]
-# print(diff_list)
-diff_sum = sum(diff_list)
-amari = k-diff_sum
-if amari < 0:
-    print("No")
-elif amari == 0:
-    print("Yes")
+a, b = list(map(int, input().split()))
+# lcm = np.lcm(a, b)
+lcm = a*b//math.gcd(a, b)
+if lcm > 10**18:
+    print("Large")
 else:
-    if amari % 2 == 0:
-        print("Yes")
-    else:
-        print("No")
+    print(lcm)
