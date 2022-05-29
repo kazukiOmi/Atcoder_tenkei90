2c6ac898144cf2a85a129e9292bd3ad5a0d307f3
Kazuki Omi <k.omi.646@stn.nitech.ac.jp>
2022-05-30 01:23:34 +0900

    44: Shift and Swqpping 末尾popからの先頭insertはshiftで高速化

--- tmp/main.py
+++ tmp/main.py
@@ -1,13 +1,22 @@
 import sys
-# import numpy as np
-import math
 
 input = sys.stdin.readline
 
-a, b = list(map(int, input().split()))
-# lcm = np.lcm(a, b)
-lcm = a*b//math.gcd(a, b)
-if lcm > 10**18:
-    print("Large")
-else:
-    print(lcm)
+n, q = list(map(int, input().split()))
+sequence = list(map(int, input().split()))
+
+shift = 0
+for i in range(q):
+    t, x, y = list(map(int, input().split()))
+    x, y = x-1, y-1
+
+    x1 = (x-shift) % n
+    y1 = (y-shift) % n
+
+    if t == 1:
+        sequence[x1], sequence[y1] = sequence[y1], sequence[x1]
+    elif t == 2:
+        shift += 1
+        # shift %= n
+    else:
+        print(sequence[x1])
