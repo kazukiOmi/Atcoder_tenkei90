65b63e427df3ccd9bfc2f8c63f363201216374fb
Kazuki Omi <k.omi.646@stn.nitech.ac.jp>
2022-05-31 22:08:58 +0900

    48: I will not drop out

--- tmp/main.py
+++ tmp/main.py
@@ -1,4 +1,3 @@
-from email.mime import base
 import sys
 
 
@@ -7,32 +6,16 @@ def base46(x):
 
 
 input = sys.stdin.readline
-# a = list(map(base46, input().split()))
-# b = list(map(base46, input().split()))
-# c = list(map(base46, input().split()))
 
-n = int(input())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-c = list(map(int, input().split()))
+n, k = list(map(int, input().split()))
 
-a_dict = {}
-b_dict = {}
-c_dict = {}
-for i in range(46):
-    a_dict[i] = 0
-    b_dict[i] = 0
-    c_dict[i] = 0
+ab_list = [list(map(int, input().split())) for _ in range(n)]
 
-
-for ai, bi, ci in zip(a, b, c):
-    at, bt, ct = ai % 46, bi % 46, ci % 46
-    a_dict[at] += 1
-    b_dict[bt] += 1
-    c_dict[ct] += 1
+a_list = [b for _, b in ab_list]
+b_list = [a-b for a, b in ab_list]
+a_list.extend(b_list)
+new_a_list = sorted(a_list, reverse=True)
 ans = 0
-for i in range(46):
-    for j in range(46):
-        base_46 = (46-(i+j)) % 46
-        ans += a_dict[i]*b_dict[j]*c_dict[base_46]
+for v in new_a_list[:k]:
+    ans += v
 print(ans)
