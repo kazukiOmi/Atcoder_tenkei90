22c25f01c84070ff840fb065d14539b692d6e255
Kazuki Omi <k.omi.646@stn.nitech.ac.jp>
2022-06-01 01:07:04 +0900

    64: uplift

--- tmp/main.py
+++ tmp/main.py
@@ -2,10 +2,24 @@ import sys
 import math
 
 
-n = int(input())
-a = [list(map(int, input().split())) for _ in range(n)]
-ans = 1
-for ai in a:
-    ans *= sum(ai)
+n, q = list(map(int, input().split()))
+a = list(map(int, input().split()))
+l = [list(map(int, input().split())) for _ in range(q)]
 
-print(ans % (10**9+7))
+diff_list = []
+for i in range(n-1):
+    diff_list.append(a[i+1]-a[i])
+
+tmp = [abs(x) for x in diff_list]
+ans = sum(tmp)
+for left, right, v in l:
+    if left > 1:
+        ans -= abs(diff_list[left-2])
+        diff_list[left-2] += v
+        ans += abs(diff_list[left-2])
+    if right < n:
+        ans -= abs(diff_list[right-1])
+        diff_list[right-1] -= v
+        ans += abs(diff_list[right-1])
+    # print(diff_list)
+    print(ans)
