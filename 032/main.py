97793cea3a0547a1bc15b438c5bf15ac0faf7740
Kazuki Omi <k.omi.646@stn.nitech.ac.jp>
2022-06-16 19:15:45 +0900

    032

---
+++ 032/main.py
@@ -0,0 +1,44 @@
+import sys
+from itertools import permutations
+
+
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+m = int(input())
+
+
+uwasa = {}
+for _ in range(m):
+    x, y = list(map(int, input().split()))
+    x, y = x-1, y-1
+    if x not in uwasa:
+        uwasa[x] = [y]
+    else:
+        uwasa[x].append(y)
+    if y not in uwasa:
+        uwasa[y] = [x]
+    else:
+        uwasa[y].append(x)
+
+
+ans = 10001
+
+for i in permutations(range(n)):
+    can_goal = True
+    for j in range(n-1):
+        if i[j] in uwasa:
+            if i[j+1] in uwasa[i[j]]:
+                can_goal = False
+                break
+    if can_goal:
+        tmp = 0
+        order = 0
+        for k in i:
+            tmp += a[k][order]
+            order += 1
+        if tmp < ans:
+            ans = tmp
+if ans < 10001:
+    print(ans)
+else:
+    print(-1)
