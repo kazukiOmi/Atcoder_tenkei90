13dcac7c742f8a91ee77a7c68f8b21d073a24626
Kazuki Omi <k.omi.646@stn.nitech.ac.jp>
2022-06-16 18:00:33 +0900

    014

---
+++ 014/main.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(input())
+home_list = list(map(int, input().split()))
+school_list = list(map(int, input().split()))
+home_list.sort()
+school_list.sort()
+
+ans = 0
+for i in range(n):
+    ans += abs(home_list[i] - school_list[i])
+print(ans)
