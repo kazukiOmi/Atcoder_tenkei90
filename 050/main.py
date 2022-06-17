a07568e25fe0bc187ba0e1825147d5b696bc4cf0
Kazuki Omi <k.omi.646@stn.nitech.ac.jp>
2022-06-17 12:45:40 +0900

    050

---
+++ 050/main.py
@@ -0,0 +1,19 @@
+def combinations_count(n, r):
+    ans = 1
+    bunbo = 1
+    s = 1
+    for i in range(n, n-r, -1):
+        ans *= i
+        bunbo *= s
+        s += 1
+    return ans // bunbo
+
+
+n, l = list(map(int, input().split()))
+ans = 0
+use_max_l = n // l
+
+for i in range(use_max_l+1):
+    total = n-(l*i)+i
+    ans += combinations_count(total, i)
+print(ans % (10**9+7))
